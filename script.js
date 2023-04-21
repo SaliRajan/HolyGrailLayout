9bea7f2354bc526adf3b48eeae2187cc6547f5ee
Sali <sali2sam@gmail.com>
2023-04-21 09:56:42 +0200

    Js Variables and Arrays - Bootstrap5 Fundamentals - Gradients and KeyFrames

---
+++ script.js
@@ -0,0 +1,21 @@
+var student1 = "Jeswin" //String
+var score = 10 //Integer
+var learningSpeed = 8.2 //Float
+var isLearningCoding = true //Boolean
+var currency = "$" //Char
+
+console.log("Student name is:" + student1)
+console.info("Student score is:" + score + "\n " + "And learningspeed is:" + learningSpeed)
+console.trace("student isLearningCoding:" + true)
+
+var students = [
+    "JOSEPH", //Index 0
+    "MIKE", //Index 1
+    "FELIX", //Index 2
+    "IVAN", //Index 3
+    "ELLA", //Index 4
+    "KORINA", //Index 5 
+    "SALI" //Index 6
+];
+
+console.log(students[6]);
